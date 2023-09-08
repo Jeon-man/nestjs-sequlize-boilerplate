c40aee228f5563f5bed06071ee666561f113de0f
Jeon-man <gusals236@gmail.com>
2023-09-08 17:50:09 +0900

    REFACTOR: module 구조 변경

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,14 +2,22 @@ import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-import { User } from './models';
+
+import { isModelClass } from '@util/sequelize';
+import * as M from './models';
+const models = Object.values(M).filter(isModelClass);
 
 @Module({
-  imports: [SequelizeModule.forFeature([User])],
+  imports: [SequelizeModule.forFeature(models)],
   providers: [UserService],
-  controllers: [UserController],
   exports: [SequelizeModule],
 })
 export class UserModule {}
 
-export default UserModule;
+@Module({
+  imports: [UserModule],
+  controllers: [UserController],
+})
+export class UserRouteModule {}
+
+export default UserRouteModule;
--- src/utils/sequelize.ts
+++ src/utils/sequelize.ts
@@ -1,3 +1,7 @@
-import { Filterable, IncludeOptions, Model } from 'sequelize';
-
+import { Filterable, IncludeOptions, ModelStatic } from 'sequelize';
+import { Model } from 'sequelize-typescript';
+import { Include, isNotNullable } from './types';
 export type IncludeOptionsWithType<T extends Model> = Omit<IncludeOptions, 'where'> & Filterable<T>;
+
+export const isModelClass = <T>(m: T): m is Include<T, ModelStatic<Model<any, any>>> =>
+  isNotNullable(m) && (m as any).prototype instanceof Model;

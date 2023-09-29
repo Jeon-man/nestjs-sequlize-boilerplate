bb2e7f87046269af4292d141eb40ce34845cfe62
Jeon-man <gusals236@gmail.com>
2023-09-29 19:03:09 +0900

    REFACTOR: module 구조 변경

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,23 +1,16 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 import { isModelClass } from '@util/sequelize';
 import * as M from './models';
+import { UserController } from './user.controller';
 const models = Object.values(M).filter(isModelClass);
 
 @Module({
   imports: [SequelizeModule.forFeature(models)],
   providers: [UserService],
+  controllers: [UserController],
   exports: [SequelizeModule],
 })
-export class UserModule {}
-
-@Module({
-  imports: [UserModule],
-  controllers: [UserController],
-})
-export class UserRouteModule {}
-
-export default UserRouteModule;
+export default class UserModule {}
